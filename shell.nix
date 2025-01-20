7f446a31623ffe019bc503c9d90d22f9fc0fa750
Liam Ederzeel <liamederzeel@gmail.com>
2025-01-20 14:41:56 +0100

    ci(nicos): fix nix files

--- shell.nix
+++ shell.nix
@@ -1,30 +1,11 @@
-{
-  description = "tmux-booster development environment";
-
-  inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    systems.url = "github:nix-systems/default";
-  };
-
-  outputs =
-    { systems, nixpkgs, ... }@inputs:
-    let
-      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
-    in
-    {
-      devShells = eachSystem (pkgs: {
-        default = pkgs.mkShell {
-          name = "tmux-booster";
-          buildInputs = with pkgs; [
-            rustc
-            cargo
-            rustfmt
-            rustPackages.clippy
-          ];
-          shellHook = ''
-            echo "node `${pkgs.nodejs}/bin/node --version`"
-          '';
-        };
-      });
-    };
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  # Get dependencies from the main package
+  inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
+  # Additional tooling
+  buildInputs = with pkgs; [
+    rust-analyzer # LSP Server
+    rustfmt       # Formatter
+    clippy        # Linter
+  ];
 }
